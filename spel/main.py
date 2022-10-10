20154d858aff656306bc66161aaae275760a2237
thamata <prilepakirill@gmail.com>
2022-10-10 14:32:46 +0200

    start meny typ

--- spel/main.py
+++ spel/main.py
@@ -2,29 +2,35 @@ import game
 
 
 def printC(str):
-    print(f'''
-    -----------------------------------------------
-    >>{str}
-    -----------------------------------------------
-    ''')
+    print(f'''-----------------------------------------------
+{str}
+-----------------------------------------------''')
 
 running = False
+game_map = game.Map()
+game_player = game.Player()
 
-'''printC("Type start to start the game")
-input = input()
-if(input.lower() == "start"):
-    printC("Game started")
-    running = True'''
+info = '''Game prompts: 
+>>[This means that the game is waiting for your input, input is not case sensitive]
+//[The text after these signs indicate a game message]'''
 
-mapM = game.Map()
-playerP = game.Player()
-#mapM.moveRight()
-currentpos = mapM.getPos()
+printC(f"Game info!\nThis is the map: \n{game_map}\nYour current position on the map is: [0]\n\n{info}\n\nTo start the game type [start]\nTo exit the game type [exit] during any input prompt")
 
-printC(f"Map:\n{mapM}")
+def player_input():
+    printC("//Which direction do you want to move: [right] [left] [up] [down]")
+    direction = input(str(">>"))
+    match direction.lower():
+        case "right":
+            game_map.moveRight()
+        case "left":
+            game_map.moveLeft()
+        case "up":
+            game_map.moveUp()
+        case "down":
+            game_map.moveDown()
+    printC(f"{game_map}\n//Your current position on the map is: [{game_map.currentpos}]")
 
-print(currentpos)
-
-print(mapM.dialog())
-print(playerP.health)
-#printC(game.Dialog)
+while running:
+    player_input()
+    print(game_map.dialog())
+    continue
