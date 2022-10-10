4233cb0fb1ca8a16e7a8c62e543cd0e6a835c719
thamata <prilepakirill@gmail.com>
2022-10-10 13:49:19 +0200

    idek, start av dialog

--- spel/game.py
+++ spel/game.py
@@ -5,19 +5,30 @@ class Player:
 
 class Map:
     def __init__(self):
+        #4x4 grid som array
         self.grid = np.array([[0,1,2,3],[4,5,6,7],[8,9,10,11],[12,13,14,15]])
 
-        self.currentpos = self.grid[1][0]
+        self.currentpos = self.grid[0][0]
     
+    def __str__(self):
+        #returnerar mappen som en sträng
+        return (str(self.grid))
+
+    def getPos(self):
+        #returnerar nuvarande position på mappen
+        return self.currentpos
+
+    #flyttar currentpos ett steg till höger
     def moveRight(self):
         toMove = 0
         for n in range(3):
+            #bug där pos kan gå över 15
             if(self.currentpos != self.grid[n][3]):
                 toMove = 1
             else:
                 return print("you cannot move further right")
         self.currentpos += toMove
-
+    #flyttar currentpos ett steg till vänster
     def moveLeft(self):
         toMove = 0
         for n in range(3):
@@ -26,36 +37,22 @@ class Map:
             else:
                 return print("you cannot move further left")
         self.currentpos -= toMove
-
+    #flyttar currentpos ett steg ner/fyra steg höger
     def moveDown(self):
         if(self.currentpos <= 11):
             self.currentpos += 4
         else:
             return print("you cannot move further down")
+    #flyttar currentpos ett steg upp/fyra steg vänster
     def moveUp(self):
         if(self.currentpos >= 4):
             self.currentpos -= 4
         else:
             return print("you cannot move further up")
-
-instans = Map()
-
-print(instans.grid)
-print("pos:", instans.currentpos)
-instans.moveRight()
-print("pos:", instans.currentpos)
-instans.moveRight()
-print("pos:", instans.currentpos)
-instans.moveRight()
-print("pos:", instans.currentpos)
-instans.moveRight()
-print("pos:", instans.currentpos)
-
-instans.moveLeft()
-print("pos:", instans.currentpos)
-instans.moveLeft()
-print("pos:", instans.currentpos)
-instans.moveLeft()
-print("pos:", instans.currentpos)
-instans.moveLeft()
-print("pos:", instans.currentpos)
+    
+    def dialog(self):
+            match self.currentpos:
+                case 0:
+                    return "square 0"
+                case 1:
+                    return "square 1"
--- spel/main.py
+++ spel/main.py
@@ -10,10 +10,21 @@ def printC(str):
 
 running = False
 
-printC("Type start to start the game")
+'''printC("Type start to start the game")
 input = input()
 if(input.lower() == "start"):
     printC("Game started")
-    running = True
+    running = True'''
 
-while running:
+mapM = game.Map()
+playerP = game.Player()
+#mapM.moveRight()
+currentpos = mapM.getPos()
+
+printC(f"Map:\n{mapM}")
+
+print(currentpos)
+
+print(mapM.dialog())
+print(playerP.health)
+#printC(game.Dialog)
